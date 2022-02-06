65d56e1bb81cfc61a487d6084c2a09070811593b
Hexxie777 <aleksanderpaszkiewicz@outlook.com>
2022-02-06 16:08:19 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #loginy do log
 login = ["Worker", "Szef","Gość"]
-#hasla do passw przy gosciu kliknij enter
+#hasla do passw 
 password = ["workspace", "imaboss", " "]
 
 accounts = {"Worker" : "Pracownik", "Szef" : "Prezes", "Gość" : "Gościa"}
@@ -12,25 +12,36 @@ print("""
         
     """)
 
+
+def games():
+    print("Co chcesz włączyć?\n1 - 'Rock Papers Scissors'\n2 - 'Find a number'\n3 - Słownik")
+    dec = int(input())
+    if dec == 1:
+        print("Włączam grę...")
+        input("Naciśnij enter, aby kontynuować...")
+        import games.game2 as rock_papers_scissors
+    if dec == 2:
+        print("Włączam grę...")
+        input("Naciśnij enter, aby kontynuować...")
+        import games.game1 as find_a_number
+    if dec == 3:
+        print("Ładuję słownik...")
+        input("Naciśnij enter, aby kontynuować...")
+        import games.slownik as slownik
+
 log = input("Zaloguj się podając login: ")
 
 passw = input("Podaj hasło: ")
 
 if log == login[0] and passw == password[0]:
     print("Witaj ", login[0]," masz permisje: ",  accounts["Worker"])
-    print("Masz dużo roboty, zrobisz coś pożytecznego co?")
-    #Importowanie gry w zależności od podanego konta
-    import games.game2 as game2
+    games()
 elif log == login[1] and passw == password[1]:
     print("Witaj ",login[1], "masz permisje: ", accounts["Szef"])
-    print("Baw się dobrze!")
-    #Importowanie gry w zależności od podanego konta
-    import games.game1 as game1
+    games()
 elif log == login[2] and password[2]:
     print("Witaj ", login[2], " masz permisje: ", accounts["Gość"])
-    print("A co tu takiego ciekawego?")
-    #Importowanie gry w zależności od podanego konta
-    import games.slownik as slownik
+    games()
 else:
-    print("Podano złe dane")
+    print("Podano złe dane.")
     
